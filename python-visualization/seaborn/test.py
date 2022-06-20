54dec202a4a2c1c69f820bc0dc38a22d819d0d77
lcdse7en <2353442022@qq.com>
2022-06-20 10:55:08 +0800

    first

--- python-visualization/seaborn/test.py
+++ python-visualization/seaborn/test.py
@@ -1,7 +1,15 @@
-from matplotlib.pyplot import plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 from pprint import pprint
 
 df = pd.read_excel('./data_xlsx/data1.xlsx')
-pprint(df)
+
+sns.set_style('darkgrid')
+plt.rcParams['font.sans-serif'] = ['SimHei']
+
+x = df['类别']
+y = df['总金额']
+plt.bar(x, y, color='slateblue', width=0.5)
+plt.show()
+# # pprint(df)
